a8804d6d10db8d0539b094ce557f353714c52a23
kadamati venkatasai <venkatasaikadamati@gmail.com>
2022-06-06 19:44:41 +0530

    Update 704. Binary Search.cpp

--- LeetCode/Study Plans/Binary Search/Binary Search I/Day 1/704. Binary Search.cpp
+++ LeetCode/Study Plans/Binary Search/Binary Search I/Day 1/704. Binary Search.cpp
@@ -1,14 +1,22 @@
-class Solution {
-  public:
-  int search(vector<int>& nums, int target) {
-    int mid, start = 0, end = nums.size() - 1;
-    while (start <= end) {
-      mid = start + (end - start) / 2;
-    // Here to make operations faster we can evaluate mid = (start + end) >>> 1;
-      if (nums[mid] == target) return mid;
-      if (target < nums[mid]) end = mid - 1;
-      else start = mid + 1;
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int mid, start = 0, end = nums.size() - 1;
+        while (start <= end)
+        {
+            mid = start + (end - start) / 2;
+            // Here to make operations faster we can evaluate mid = (start + end) >>> 1;
+            if (nums[mid] == target)
+                return mid;
+            if (target < nums[mid])
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return -1;
     }
-    return -1;
-  }
 };
+
+// Note: check the mid operation overflow problem by avoiding direct evaluation of mid to start+end/2;
